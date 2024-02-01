7e1c839470faf26da55679260411c064f80a0c5f
Jose Cuevas <josecueram@gmail.com>
2024-02-01 10:52:32 -0600

    Add overflow scrolling to layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
 export default function Home() {
   return (
-    <main className="flex bg-gray-700 min-h-screen text-white">
-      <div className="bg-gray-800 p-4">
-        <div className="h-12 w-12 bg-white rounded-full flex items-center justify-center text-gray-800">
-          JC
-        </div>
+    <main className="flex text-white h-screen">
+      <div className="bg-gray-800 p-3 space-y-2 overflow-y-scroll">
+        {[...Array(40)].map((_, index) => (
+          <div
+            key={index}
+            className="h-12 w-12 bg-white rounded-full flex items-center justify-center text-gray-800"
+          >
+            {index}
+          </div>
+        ))}
       </div>
       <div className="bg-gray-700 w-60 flex flex-col">
-        <div className="p-4 shadow-md">Tailwind CSS</div>
-        <div className="p-4 flex-1">Channels</div>
+        <div className="px-3 h-12 flex items-center shadow-md">
+          Tailwind CSS
+        </div>
+        <div className="p-3 flex-1 overflow-y-scroll space-y-2">
+          {[...Array(40)].map((_, index) => (
+            <p key={index}>Channel {index}</p>
+          ))}
+        </div>
       </div>
       <div className="bg-gray-600 flex-1 flex flex-col">
-        <div className="p-4 shadow-md">#general</div>
-        <div className="p-4 flex-1">Messages</div>
+        <div className="px-3 h-12 flex items-center shadow-md">#general</div>
+        <div className="p-3 flex-1 overflow-y-scroll space-y-4">
+          {[...Array(40)].map((_, index) => (
+            <p key={index}>
+              Somewhere, somewhere, somewhere to go. Many years ago. {index}
+            </p>
+          ))}
+        </div>
       </div>
     </main>
   );
