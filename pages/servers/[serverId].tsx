2a97516693f86af29db1290f77167d04caecd4e9
Jose Cuevas <josecueram@gmail.com>
2024-02-02 11:03:33 -0600

    Update styles of the server selector

--- pages/servers/[serverId].tsx
+++ pages/servers/[serverId].tsx
@@ -1,14 +1,19 @@
 import { useRouter } from "next/router";
-
+import { VerifiedIcon, CheckIcon, ChevronIcon } from "@/components/icons";
 export default function Server1() {
   const router = useRouter();
 
   return (
     <>
       <div className="bg-gray-800 w-60 flex flex-col">
-        <div className="font-title text-white px-3 h-12 flex items-center shadow-sm">
+        <button className="font-title text-white text-[15px] px-4 h-12 flex items-center shadow-sm transition hover:bg-gray-550/[.16]">
+          <div className="relative h-4 w-4 mr-1">
+            <VerifiedIcon className="absolute h-4 w-4 text-gray-550" />
+            <CheckIcon className="absolute h-4 w-4" />
+          </div>
           Gain
-        </div>
+          <ChevronIcon className="h-[18px] w-[18px] ml-auto opacity-80" />
+        </button>
         <div className="text-gray-300 font-medium p-3 flex-1 overflow-y-scroll space-y-2 ">
           <p className="text-white">Channel (unread)</p>
           {[...Array(40)].map((_, index) => (
