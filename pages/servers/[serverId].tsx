0fe3ada641183861a3a7e9e6be1f68df8ed6c3a2
Jose Cuevas <josecueram@gmail.com>
2024-02-02 10:35:33 -0600

    Update `pages/server/` to use dynamic route

---
+++ pages/servers/[serverId].tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+
+export default function Server1() {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="bg-gray-800 w-60 flex flex-col">
+        <div className="font-title text-white px-3 h-12 flex items-center shadow-md">
+          Server {router.query.serverId}
+        </div>
+        <div className="text-gray-300 font-medium p-3 flex-1 overflow-y-scroll space-y-2 ">
+          <p className="text-white">Channel (unread)</p>
+          {[...Array(40)].map((_, index) => (
+            <p key={index}>Channel {index}</p>
+          ))}
+        </div>
+      </div>
+      <div className="bg-gray-700 flex-1 flex flex-col">
+        <div className="px-3 h-12 flex items-center shadow-md">#general</div>
+        <div className="p-3 flex-1 overflow-y-scroll space-y-4">
+          {[...Array(40)].map((_, index) => (
+            <p key={index}>
+              Somewhere, somewhere, somewhere to go. Many years ago. {index}
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
